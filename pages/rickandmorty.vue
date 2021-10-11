c4138097ff5cdeb8c9ff147175011fd367b2e756
Sotelo <cgonzalezsotelo@gmail.com>
2021-10-10 23:33:02 -0300

    desafio nuxt

---
+++ pages/rickandmorty.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <v-row>
+      <v-col v-for="(personaje, index) in personajes" :key="index">
+        <v-card>
+          <v-img height="250" :src="personaje.imagen"></v-img>
+
+          <v-card-title>{{ personaje.nombre }}</v-card-title>
+
+          <v-divider class="mx-4"></v-divider>
+        </v-card>
+      </v-col>
+    </v-row>
+  </main>
+</template>
+<script>
+export default {
+  data: () => ({
+    personajes: [
+      {
+        id: 1,
+        nombre: 'Morty',
+        imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+      },
+      {
+        id: 2,
+        nombre: 'Rick',
+        imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      },
+      {
+        id: 3,
+        nombre: 'Summer',
+        imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+      },
+      {
+        id: 4,
+        nombre: 'Beth',
+        imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+      },
+      {
+        id: 5,
+        nombre: 'Jerry',
+        imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+      },
+    ],
+  }),
+}
+</script>
